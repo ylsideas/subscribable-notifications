d8a927c0f7609644952cb3e57a930a2c0710f79c
Peter Fox <peter.fox@peterfox.me>
2019-10-30 23:00:09 +0000

    Fixes a parameter name

--- src/Contracts/CanUnsubscribe.php
+++ src/Contracts/CanUnsubscribe.php
@@ -4,5 +4,5 @@ namespace YlsIdeas\SubscribableNotifications\Contracts;
 
 interface CanUnsubscribe
 {
-    public function unsubscribeLink(?string $service = null): string;
+    public function unsubscribeLink(?string $mailingList = null): string;
 }
